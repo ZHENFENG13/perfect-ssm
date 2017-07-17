9a5d21f750dc0648cb1b3482cd8f230646ebc024
shuaihan <1034683568@qq.com>
2017-07-17 18:34:27 +0800

    :art: UPDATE PictureController

--- src/main/java/com/ssm/promotion/core/admin/PictureController.java
+++ src/main/java/com/ssm/promotion/core/admin/PictureController.java
@@ -1,5 +1,8 @@
 package com.ssm.promotion.core.admin;
 
+import com.ssm.promotion.core.common.Result;
+import com.ssm.promotion.core.common.ResultGenerator;
+import com.ssm.promotion.core.entity.Article;
 import com.ssm.promotion.core.entity.PageBean;
 import com.ssm.promotion.core.entity.Picture;
 import com.ssm.promotion.core.service.PictureService;
@@ -9,8 +12,7 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -19,117 +21,166 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/picture")
+@RequestMapping("/pictures")
 public class PictureController {
-	@Resource
-	private PictureService pictureService;
-	private static final long serialVersionUID = 1L;
-	private static final Logger log = Logger.getLogger(PictureController.class);// 日志文件
-	/**
-	 * 查找相应的数据集合
-	 * 
-	 * @param page
-	 * @param rows
-	 * @param picture
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/list")
-	public String list(
-			@RequestParam(value = "page", required = false) String page,
-			@RequestParam(value = "rows", required = false) String rows,
-			Picture picture, HttpServletResponse response) throws Exception {
-		Map<String, Object> map = new HashMap<String, Object>();
-		if (page != null && rows != null) {
-			PageBean pageBean = new PageBean(Integer.parseInt(page),
-					Integer.parseInt(rows));
-			map.put("start", pageBean.getStart());
-			map.put("size", pageBean.getPageSize());
-		}
-		if (picture != null) {
-			map.put("id", picture.getId() + "");
-			map.put("type", picture.getType() + "");
-			map.put("grade", picture.getGrade() + "");
-		}
-		List<Picture> pictureList = pictureService.findPicture(map);
-		Long total = pictureService.getTotalPicture(map);
-		JSONObject result = new JSONObject();
-		JSONArray jsonArray = JSONArray.fromObject(pictureList);
-		result.put("rows", jsonArray);
-		result.put("total", total);
-		log.info("request: picture/list , map: " + map.toString());
-		ResponseUtil.write(response, result);
-		return null;
-	}
+    @Resource
+    private PictureService pictureService;
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = Logger.getLogger(PictureController.class);// 日志文件
 
-	/**
-	 * 保存或修改
-	 * 
-	 * @param picture
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/save")
-	public String save(Picture picture, HttpServletResponse response)
-			throws Exception {
-		int resultTotal = 0; 
-		if (picture.getId() == null) {
-			picture.setTime(DateUtil.getCurrentDateStr());
-			resultTotal = pictureService.addPicture(picture);
-		} else {
-			System.out.println(picture.getPath());
-			resultTotal = pictureService.updatePicture(picture);
-		}
-		JSONObject result = new JSONObject();
-		if (resultTotal > 0) { 
-			result.put("success", true);
-		} else {
-			result.put("success", false);
-		}
-		log.info("request: picture/save ,  " + picture.toString());
-		ResponseUtil.write(response, result);
-		return null;
-	}
+    /**
+     * 查找相应的数据集合
+     *
+     * @param page
+     * @param rows
+     * @param picture
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Result list(
+            @RequestParam(value = "page", required = false) String page,
+            @RequestParam(value = "rows", required = false) String rows,
+            Picture picture, HttpServletResponse response) throws Exception {
+        Map<String, Object> map = new HashMap<String, Object>();
+        if (page != null && rows != null) {
+            PageBean pageBean = new PageBean(Integer.parseInt(page),
+                    Integer.parseInt(rows));
+            map.put("start", pageBean.getStart());
+            map.put("size", pageBean.getPageSize());
+        }
+        if (picture != null) {
+            map.put("id", picture.getId() + "");
+            map.put("type", picture.getType() + "");
+            map.put("grade", picture.getGrade() + "");
+        }
+        List<Picture> pictureList = pictureService.findPicture(map);
+        Result result = ResultGenerator.genSuccessResult();
+        Long total = pictureService.getTotalPicture(map);
+        Map data = new HashMap();
+        data.put("rows", pictureList);
+        data.put("total", total);
+        log.info("request: picture/list , map: " + map.toString());
+        result.setData(data);
+        return result;
+    }
 
-	/**
-	 * 删除
-	 * 
-	 * @param ids
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/delete")
-	public String delete(@RequestParam(value = "ids") String ids,
-			HttpServletResponse response) throws Exception {
-		JSONObject result = new JSONObject();
-		String[] idsStr = ids.split(",");
-		for (int i = 0; i < idsStr.length; i++) {
-			pictureService.deletePicture(idsStr[i]);
-		}
-		result.put("success", true);
-		log.info("request: picture/delete , ids: " + ids);
-		ResponseUtil.write(response, result);
-		return null;
-	}
+    /**
+     * 保存或修改
+     *
+     * @param picture
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/save")
+    public String save(Picture picture, HttpServletResponse response)
+            throws Exception {
+        int resultTotal = 0;
+        if (picture.getId() == null) {
+            picture.setTime(DateUtil.getCurrentDateStr());
+            resultTotal = pictureService.addPicture(picture);
+        } else {
+            resultTotal = pictureService.updatePicture(picture);
+        }
+        JSONObject result = new JSONObject();
+        if (resultTotal > 0) {
+            result.put("success", true);
+        } else {
+            result.put("success", false);
+        }
+        log.info("request: picture/save ,  " + picture.toString());
+        ResponseUtil.write(response, result);
+        return null;
+    }
 
-	/**
-	 * 根据id查找
-	 * 
-	 * @param id
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/findById")
-	public String findById(@RequestParam(value = "id") String id,
-			HttpServletResponse response) throws Exception {
-		Picture picture = pictureService.findById(id);
-		JSONObject jsonObject = JSONObject.fromObject(picture);
-		log.info("request: picture/findById");
-		ResponseUtil.write(response, jsonObject);
-		return null;
-	}
+    /**
+     * 添加
+     *
+     * @param picture
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
+    public Result save(Picture picture)
+            throws Exception {
+        int resultTotal = 0;
+
+        picture.setTime(DateUtil.getCurrentDateStr());
+
+        resultTotal = pictureService.addPicture(picture);
+
+        log.info("request: picture/save , " + picture.toString());
+
+        if (resultTotal > 0) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("添加失败");
+        }
+    }
+
+    /**
+     * 修改
+     *
+     * @param picture
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    @ResponseBody
+    public Result update(Picture picture)
+            throws Exception {
+        int resultTotal = 0;
+
+        resultTotal = pictureService.updatePicture(picture);
+
+        log.info("request: picture/update , " + picture.toString());
+
+        if (resultTotal > 0) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("修改失败");
+        }
+    }
+
+
+    /**
+     * 删除
+     *
+     * @param ids
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/{ids}", method = RequestMethod.DELETE)
+    public Result delete(@PathVariable(value = "ids") String ids
+    ) throws Exception {
+        String[] idsStr = ids.split(",");
+        for (int i = 0; i < idsStr.length; i++) {
+            pictureService.deletePicture(idsStr[i]);
+        }
+        log.info("request: picture/delete , ids: " + ids);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    /**
+     * 根据id查找
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable("id") String id) throws Exception {
+        Picture picture = pictureService.findById(id);
+        log.info("request: picture/findById");
+        if (picture != null) {
+            Result result = ResultGenerator.genSuccessResult();
+            result.setData(picture);
+            return result;
+        } else {
+            return ResultGenerator.genFailResult("无资源");
+        }
+    }
 }
