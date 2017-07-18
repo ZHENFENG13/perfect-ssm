cd37d2f321f1577e92b82c14e41faaa32b1b05eb
shuaihan <1034683568@qq.com>
2017-07-18 11:38:47 +0800

    :art: UPDATE UserController

--- src/main/java/com/ssm/promotion/core/admin/UserController.java
+++ src/main/java/com/ssm/promotion/core/admin/UserController.java
@@ -57,6 +57,7 @@ public class UserController {
         if (resultUser == null) {
             return ResultGenerator.genFailResult("请认真核对账号、密码！");
         } else {
+            resultUser.setPassword("PASSWORD");
             Map data = new HashMap();
             data.put("currentUser", resultUser);
             return ResultGenerator.genSuccessResult(data);
