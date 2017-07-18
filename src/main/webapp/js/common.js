917b056f579d8403164d6e20125b7214025a747f
shuaihan <1034683568@qq.com>
2017-07-18 11:43:36 +0800

    :tada: ADD cookie function

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,29 +1,33 @@
-// »ñÈ¡µ±Ç°ÈÕÆÚÊ±¼ä
-function getCurrentDateTime() {
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    return year + "-" + formatZero(month) + "-" + formatZero(day) + " " + formatZero(hours) + ":" + formatZero(minutes) + ":" + formatZero(seconds);
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
 }
 
-// »ñÈ¡µ±Ç°ÈÕÆÚ
-function getCurrentDate() {
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
-    return year + "-" + formatZero(month) + "-" + formatZero(day);
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
 }
 
+function delCookie(name) {
+    var exp = new Date();
+    exp.setTime(exp.getTime() - 1);
+    var cval = getCookie(name);
+    if (cval != null)
+        document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+}
 
-function formatZero(n) {
-    if (n >= 0 && n <= 9) {
-        return "0" + n;
-    } else {
-        return n;
+function checkCookie() {
+    if (getCookie("userName") == null || getCookie("roleName") == null) {
+        alert("æœªç™»å½•!");
+        window.location.href = "login.jsp";
+    }
+    else {
+        alert("userName:" + getCookie("userName"));
     }
 }
+
