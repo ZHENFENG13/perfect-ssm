169a1758a49baf9e3eac1c35f0a105a5735bc1fc
shuaihan <1034683568@qq.com>
2017-08-10 22:47:08 +0800

    :art::memo: UPDATE TestModule

--- src/test/java/controller/test/ArticleControllerTest.java
+++ src/test/java/controller/test/ArticleControllerTest.java
@@ -1,9 +1,13 @@
 package controller.test;
 
+import com.alibaba.fastjson.JSONObject;
+import com.ssm.promotion.core.entity.Article;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -36,12 +40,27 @@ public class ArticleControllerTest {
     }
 
     @Test
-    public void testList() throws Exception {
-        //创建书籍列表的请求
+    public void testArticleList() throws Exception {
+        //创建文章列表的请求
         //请求方式为get
-        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders.get("/article/list.do");
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders.request(HttpMethod.GET, "/articles");
         //此请求并不需要添加请求参数
         mockMvc.perform(mockHttpServletRequestBuilder).andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    public void testArticleAdd() throws Exception {
+        //创建文章对象
+        Article article = new Article();
+        article.setAddName("13");
+        article.setArticleTitle("title");
+        article.setArticleContent("content");
+        String requestParam = JSONObject.toJSONString(article);
+        //请求方式为post
+        MockHttpServletRequestBuilder mockHttpServletRequestBuilder = MockMvcRequestBuilders.post("/articles");
+        mockMvc.perform(mockHttpServletRequestBuilder.contentType(MediaType.APPLICATION_JSON).content(requestParam)).andExpect(status().isOk())
                 .andDo(print());
 
     }
