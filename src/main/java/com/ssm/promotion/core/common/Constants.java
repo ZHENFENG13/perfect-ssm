484046a2c34c26d5095138a3b099db34edce2c93
13 <1034683568@qq.com>
2018-01-17 16:07:06 +0800

    :art: ADD cache key

--- src/main/java/com/ssm/promotion/core/common/Constants.java
+++ src/main/java/com/ssm/promotion/core/common/Constants.java
@@ -9,4 +9,7 @@ public class Constants {
     public static final int RESULT_CODE_BAD_REQUEST = 412;  // bad request
     public static final int RESULT_CODE_SERVER_ERROR = 500;  // 没有对应结果
 
+    public static final String ARTICLE_CACHE_KEY = "perfect-ssm:article:";//文章key
+    public static final String PICTURE_CACHE_KEY = "perfect-ssm:picture:";//图片key
+
 }
