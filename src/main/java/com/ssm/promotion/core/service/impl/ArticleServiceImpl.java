00701940f1c79459028c590470c0d35a6d31f1dd
13 <1034683568@qq.com>
2018-02-02 16:46:41 +0800

    ️:zap: redis code

--- src/main/java/com/ssm/promotion/core/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/ssm/promotion/core/service/impl/ArticleServiceImpl.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import com.ssm.promotion.core.common.Constants;
 import com.ssm.promotion.core.dao.ArticleDao;
 import com.ssm.promotion.core.entity.Article;
+import com.ssm.promotion.core.redis.RedisUtil;
 import com.ssm.promotion.core.service.ArticleService;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,8 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Resource
     private ArticleDao articleDao;
+    @Resource
+    private RedisUtil redisUtil;
 
     @Override
     public List<Article> findArticle(Map<String, Object> map) {
@@ -32,7 +36,12 @@ public class ArticleServiceImpl implements ArticleService {
         if (article.getArticleTitle() == null || article.getArticleContent() == null || getTotalArticle(null) > 90 || article.getArticleContent().length() > 50000) {
             return 0;
         }
-        return articleDao.insertArticle(article);
+        if (articleDao.insertArticle(article) > 0) {
+            //向mysql数据库中插入成功后，存入redis中
+            redisUtil.put(Constants.ARTICLE_CACHE_KEY + article.getId(), article);
+            return 1;
+        }
+        return 0;
     }
 
     @Override
@@ -40,7 +49,13 @@ public class ArticleServiceImpl implements ArticleService {
         if (article.getArticleTitle() == null || article.getArticleContent() == null || getTotalArticle(null) > 90 || article.getArticleContent().length() > 50000) {
             return 0;
         }
-        return articleDao.updArticle(article);
+        if (articleDao.updArticle(article) > 0) {
+            //向mysql数据库中修改成功后，修改redis中的数据
+            redisUtil.del(Constants.ARTICLE_CACHE_KEY + article.getId());
+            redisUtil.put(Constants.ARTICLE_CACHE_KEY + article.getId(), article);
+            return 1;
+        }
+        return 0;
     }
 
     @Override
@@ -50,7 +65,21 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public Article findById(String id) {
-        return articleDao.getArticleById(id);
+        //首先通过redis查询
+        //此种写法存在缓存击穿问题
+        Article article = (Article) redisUtil.get(Constants.ARTICLE_CACHE_KEY + id, Article.class);
+        //如果缓存中已经存在则直接返回，不再通过mysql数据库
+        if (article != null) {
+            return article;
+        }
+        //如果缓存中不存在或者已过期，则依然通过mysql数据库查询
+        Article articleFromMysql = articleDao.getArticleById(id);
+        if (articleFromMysql != null) {
+            //查询到数据后，存入redis缓存中
+            redisUtil.put(Constants.ARTICLE_CACHE_KEY + articleFromMysql.getId(), articleFromMysql);
+            return articleFromMysql;
+        }
+        return null;
     }
 
 }
